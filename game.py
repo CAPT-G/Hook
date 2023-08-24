3a858f705fe7e296d585ed6134e8cb79d511b2e9
CAPTAIN G <142211497+CAPT-G@users.noreply.github.com>
2023-08-23 23:28:38 -0400

    Update game.py

--- game.py
+++ game.py
@@ -22,6 +22,7 @@ users = {
 }
 
 class RandomNumberSelector:
+    def __init__(self, odds):
     """
     A class to generate random numbers based on given odds.
 
@@ -30,7 +31,6 @@ class RandomNumberSelector:
     Args:
         odds (int): The odds of selecting a number, represented as a percentage.
     """
-    def __init__(self, odds):
         self.odds = odds
         
     def generate_number(self):
@@ -104,7 +104,30 @@ class CasinoGame:
             print(f"You cashed out {cash_out_amount} coins.")
         else:
             print("You don't have enough winnings to cash out.")
-
+    def main_menu(self):
+         """
+    Display the main menu and handle user actions.
+    
+    This method presents a menu with options for a user, such as playing a game,
+    cashing out winnings, and quitting. It handles the user's input and directs them
+    to the appropriate actions.
+    """
+        while True:
+            print("\nMain Menu:")
+            print("1. Play Game")
+            print("2. Cash Out")
+            print("3. Quit")
+            choice = input("Select an option: ")
+            
+            if choice == "1":
+                self.play_game()
+            elif choice == "2":
+                self.cash_out()
+            elif choice == "3":
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please select a valid option.")
 def admin_login(username, password):
     """
     Authenticate an admin user.
@@ -148,7 +171,7 @@ def main():
     This function welcomes the user to the Casino Game, provides a menu to select a role
     (admin/player/quit), and directs the user to the appropriate menus based on their choice.
     """
-    print('Welcome to the Casino Game!')
+    print('CAPT-G!')
     current_time = time.ctime()
     print(f'Current time: {current_time}')
     while True:
@@ -172,26 +195,33 @@ def main():
             break
         else:
             print('Invalid choice. Please select a valid role.')
-def admin_menu(username):
+def admin_menu(username, game):
     """
     Display the admin menu and handle admin actions.
     
-    This function presents a menu with options for an admin user, such as managing player coins
-    and logging out. It handles the user's input and directs them to the appropriate actions.
+    This function presents a menu with options for an admin user, such as managing player coins,
+    adjust playable odds and logging out.
+    It handles the user's input and directs them to the appropriate actions.
     
     Args:
         username (str): The username of the admin user.
+        game (CasinoGame): An instance of the CasinoGame class.
     """
     print(f'Welcome, {username} (Administrator)!')
     while True:
         print('\nAdmin Menu:')
         print('1. Manage Coins for Players')
-        print('2. Log Out')
+        print('2. Adjust Playable Odds')
+        print('3. Log Out')
         choice = input('Select an option: ')
         
         if choice == '1':
             manage_coins()
         elif choice == '2':
+            new_odds = int(input("Enter new playable odds: "))
+            game.adjust_playable_odds(new_odds)
+            print(f"Playable odds changed to {new_odds}%")
+        elif choice == '3':
             print(f'Goodbye, {username}!')
             break
         else:
@@ -205,14 +235,69 @@ def manage_coins():
     action, it either adds or cashes out the specified number of coins for the player.
     """
     username = input('Enter player username: ')
+if username in users and users[username]['role'] == 'player':
     action = input('Enter action (add/cashout): ')
     coins = int(input('Enter coins: '))
-    
     if action == 'add':
+        users[username]['coins'] += coins
+        print(f'{coins} coins added to {username}.')
         add_coins(username, coins)
     elif action == 'cashout':
         cash_out_coins(username, coins)
     else:
         print('Invalid action.')
+def add_coins(username, amount):
+ """
+    Add coins to a player's account.
+    
+    This function allows an admin user to add a specified amount of coins to a player's account.
+    
+    Args:
+        username (str): The username of the player.
+        amount (int): The number of coins to be added.
+ """
+    if username in users and users[username]['role'] == 'player':
+        users[username]['coins'] += amount
+def cash_out_coins(username, amount):
+ """
+    Cash out coins from a player's account.
+    
+    This function allows an admin user to cash out a specified amount of coins from a player's account.
+    
+    Args:
+        username (str): The username of the player.
+        amount (int): The number of coins to be cashed out.
+ """
+    if username in users and users[username]['role'] == 'player' and users[username]['coins'] >= amount:
+        users[username]['coins'] -= amount
+def player_menu(username):
+    """
+    Display the player menu and handle player actions.
+    
+    This function presents a menu with options for a player user, such as playing a game,
+    cashing out winnings, and logging out. It handles the user's input and directs them
+    to the appropriate actions.
+    
+    Args:
+        username (str): The username of the player user.
+    """
+    game = CasinoGame()  # Initialize the game instance
+    print(f'Welcome, {username} (Player)!')
+    while True:
+        print('\nPlayer Menu:')
+        print('1. Play Game')
+        print('2. Cash Out')
+        print('3. Log Out')
+        choice = input('Select an option: ')
+        
+        if choice == '1':
+            game.play_game()
+        elif choice == '2':
+            game.cash_out()
+        elif choice == '3':
+            print(f'Goodbye, {username}!')
+            break
+        else:
+            print('Invalid choice. Please select a valid option.')
 if __name__ == '__main__':
-    main()
+    main()  # Start the program execution by calling the main() function
